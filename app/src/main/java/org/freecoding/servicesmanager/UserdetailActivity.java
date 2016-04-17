733707ab154c81cee73edb6e6ea83213322b4fc1
shuaihao123 <shuaihao201314@163.com>
2016-04-17 17:04:10 +0800

    asdad

---
+++ app/src/main/java/org/freecoding/servicesmanager/UserdetailActivity.java
@@ -0,0 +1,13 @@
+package org.freecoding.servicesmanager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UserdetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userdetail);
+    }
+}
