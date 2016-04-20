fce2d009551cecd22ddb3bfdc4c3129a61a04d60
shuaihao123 <shuaihao201314@163.com>
2016-04-20 16:59:34 +0800

    shouye

---
+++ app/src/main/java/org/freecoding/servicesmanager/SelectedMenuOrderActivity.java
@@ -0,0 +1,15 @@
+package org.freecoding.servicesmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * 我的订单/点餐订单
+ */
+public class SelectedMenuOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_menu_order);
+    }
+}
