4de01c04f9bea819c3f503acde59f59305c0e4a5
wowser <775594040@qq.com>
2016-04-21 16:19:12 +0800

    update
    add custom view

---
+++ app/src/main/java/org/freecoding/servicesmanager/view/MultiLineEditText.java
@@ -0,0 +1,49 @@
+package org.freecoding.servicesmanager.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class MultiLineEditText extends EditText {
+
+    private static final String TAG = "MultiLineEditText";
+
+    private Rect mRect;
+    private Paint mPaint;
+
+    // we need this constructor for LayoutInflater
+    public MultiLineEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mRect = new Rect();
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(0x80999999);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int count = getLineCount();
+        if (count < 100) {
+            count = 100;
+        }
+        Rect r = mRect;
+        Paint paint = mPaint;
+        int lineHeight = getLineHeight();
+        int baseline = getLineBounds(0, r);
+        for (int i = 0; i < count; i++) {
+            if (i >= 100) {
+                baseline = getLineBounds(i, r);
+            }
+
+            int floatY = lineHeight * (i + 1);
+            canvas.drawLine(r.left, floatY, r.right, floatY, paint);
+            canvas.save();
+        }
+        super.onDraw(canvas);
+    }
+
+}
