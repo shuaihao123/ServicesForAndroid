73b7342762cd476b8b77b456dc44fa78ba0efae6
shuaihao123 <shuaihao201314@163.com>
2016-04-21 11:11:36 +0800

    diancan

---
+++ app/src/main/java/org/freecoding/servicesmanager/GreenRestaurantActivity.java
@@ -0,0 +1,15 @@
+package org.freecoding.servicesmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * 绿色餐厅
+ */
+public class GreenRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_green_restaurant);
+    }
+}
