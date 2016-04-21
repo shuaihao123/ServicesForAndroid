89c14b90a5bbe3e864332b53dcb9098a82b1c569
wowser <775594040@qq.com>
2016-04-22 00:02:12 +0800

    update

---
+++ app/src/main/java/org/freecoding/servicesmanager/MapActivity.java
@@ -0,0 +1,270 @@
+package org.freecoding.servicesmanager;
+
+import android.location.Geocoder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CameraPosition;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.LatLngBounds;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.geocoder.GeocodeResult;
+import com.amap.api.services.geocoder.GeocodeSearch;
+import com.amap.api.services.geocoder.RegeocodeQuery;
+import com.amap.api.services.geocoder.RegeocodeResult;
+
+import java.util.List;
+
+public class MapActivity extends AppCompatActivity implements AMap.OnMapLoadedListener, AMap.OnCameraChangeListener, GeocodeSearch.OnGeocodeSearchListener, LocationSource,AMapLocationListener {
+
+
+    private GeocodeSearch geocoderSearch;
+    private MarkerOptions markerOption;
+
+    private LocationSource.OnLocationChangedListener mListener;
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+
+    private AMap aMap;
+    private MapView mapView;
+    private int x;
+    private int y;
+
+    private LatLng latlng = new LatLng(34.341568, 108.940174);// 西安市经纬度
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState); // 此方法必须重写
+
+        mapView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                x = mapView.getMeasuredWidth() / 2;
+                y = mapView.getMeasuredHeight() / 2;
+                addMarkersToMap();
+            }
+        });
+
+        init();
+    }
+
+    /**
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+            setUpMap();
+        }
+    }
+
+    private void setUpMap() {
+        aMap.setOnMapLoadedListener(this);// 设置amap加载成功事件监听器
+        aMap.setOnCameraChangeListener(this);//设置Camera移动事件监听器
+
+        aMap.setLocationSource(this);// 设置定位监听
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+        //addMarkersToMap();// 往地图上添加marker
+    }
+
+    private void addMarkersToMap() {
+
+        markerOption = new MarkerOptions();
+
+        markerOption.position(latlng).icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).draggable(false);
+
+        //aMap.addMarker(markerOption);
+
+
+        Marker marker = aMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .draggable(true));
+        marker.setPositionByPixels(x, y);
+        marker.showInfoWindow();// 设置默认显示一个infowinfow
+    }
+
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+        deactivate();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        if (null != mlocationClient) {
+            mlocationClient.onDestroy();
+        }
+    }
+
+    /**
+     * 监听amap地图加载成功事件回调
+     */
+    @Override
+    public void onMapLoaded() {
+        // 设置所有maker显示在当前可视区域地图中
+        LatLngBounds bounds = new LatLngBounds.Builder().include(latlng).build();
+        aMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 150));
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+        LatLng target = cameraPosition.target;
+        System.out.println(target.latitude + "onCameraChange------" + target.longitude);
+    }
+
+    @Override
+    public void onCameraChangeFinish(CameraPosition cameraPosition) {
+        LatLng target = cameraPosition.target;
+        System.out.println(target.latitude + "onCameraChangeFinish------" + target.longitude);
+
+        LatLonPoint latLonPoint = new LatLonPoint(target.latitude, target.longitude);
+
+        RegeocodeQuery query = new RegeocodeQuery(latLonPoint, 3000, GeocodeSearch.AMAP);
+
+
+        geocoderSearch = new GeocodeSearch(this);
+        geocoderSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
+            @Override
+            public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+                List<PoiItem> poiItems = regeocodeResult.getRegeocodeAddress().getPois();
+                for (PoiItem poiItem : poiItems) {
+                    Log.v("onRegeocodeSearched", poiItem.getTitle());//输出周边poi的信息
+                }
+            }
+
+            @Override
+            public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+
+            }
+        });
+
+        geocoderSearch.getFromLocationAsyn(query);
+    }
+
+    @Override
+    public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+        if (i == 0) {
+            List<PoiItem> poiItems = regeocodeResult.getRegeocodeAddress().getPois();
+            for (PoiItem poiItem : poiItems) {
+                Log.v("onRegeocodeSearched", poiItem.getTitle());//输出周边poi的信息
+            }
+        }
+    }
+
+    @Override
+    public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+
+    }
+
+
+    /**
+     * 定位成功后回调函数
+     */
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (mListener != null && amapLocation != null) {
+            if (amapLocation != null
+                    && amapLocation.getErrorCode() == 0) {
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+            } else {
+                //定位失败
+            }
+        }
+        LatLng latlng = new LatLng(amapLocation.getLatitude(), amapLocation.getLongitude());
+        LatLngBounds bounds = new LatLngBounds.Builder().include(latlng).build();
+        aMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 150));
+    }
+
+    /**
+     * 激活定位
+     */
+    @Override
+    public void activate(LocationSource.OnLocationChangedListener listener) {
+        mListener = listener;
+        if (mlocationClient == null) {
+            mlocationClient = new AMapLocationClient(this);
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置是否只定位一次,默认为false
+            mLocationOption.setOnceLocation(true);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mlocationClient.startLocation();
+        }
+    }
+
+    /**
+     * 停止定位
+     */
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+
+}
